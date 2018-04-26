efeea3f61d1405de54957c919a50dfc5cee5bf6d
wangty <1002814312@qq.com>
2018-04-26 18:53:26 +0800

    python3打包工具

---
+++ PyInstaller-3.3.1打包工具/bootloader/build/c4che/build.config.py
@@ -0,0 +1,2 @@
+version = 0x1090d00
+tools = [{'tooldir': None, 'tool': 'ar', 'funs': None}, {'tooldir': None, 'tool': 'c', 'funs': None}, {'tooldir': None, 'tool': 'gcc', 'funs': None}, {'tooldir': None, 'tool': 'compiler_c', 'funs': None}, {'tooldir': ['tools'], 'tool': 'strip', 'funs': None}]
